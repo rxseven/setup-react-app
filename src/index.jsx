116884d39e10cd82919a24315cb3372c3079ecf9
Theerawat Pongsupawt (MBP) <rxseven.com@gmail.com>
2018-06-21 14:57:32 +0700

    Add error handler

--- src/index.jsx
+++ src/index.jsx
@@ -10,8 +10,16 @@ import App from './components/core/App';
 // Constants
 import HTML from './constants/elements/html';
 
+// Initialize root HTML element
+const root = document.getElementById(HTML.root);
+
+// Validate root element
+if (root == null) {
+  throw new Error('No root element found');
+}
+
 // Render React element into the DOM
-ReactDOM.render(<App />, document.getElementById(HTML.root));
+ReactDOM.render(<App />, root);
 
 // Service worker
 registerServiceWorker();
