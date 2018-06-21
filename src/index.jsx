2cde32a7475cb6d4ab7158d0a7310fd485e8d213
Theerawat Pongsupawt (MBP) <rxseven.com@gmail.com>
2018-06-21 14:42:38 +0700

    Create App component

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,11 @@ import ReactDOM from 'react-dom';
 
 import registerServiceWorker from './registerServiceWorker';
 
+// Starting point component
+import App from './components/core/App';
+
 // Render React element into the DOM
-ReactDOM.render(<h1>Welcome to React</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // Service worker
 registerServiceWorker();
