89edd0eb3e78561a765c4e26b88c87f4acd5afcd
Theerawat Pongsupawt (MBP) <rxseven.com@gmail.com>
2018-06-21 14:49:03 +0700

    Specify root HTML element to the project starting point

--- src/index.jsx
+++ src/index.jsx
@@ -7,8 +7,11 @@ import registerServiceWorker from './registerServiceWorker';
 // Starting point component
 import App from './components/core/App';
 
+// Constants
+import HTML from './constants/elements/html';
+
 // Render React element into the DOM
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById(HTML.root));
 
 // Service worker
 registerServiceWorker();
