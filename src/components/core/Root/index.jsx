6cedd5cc89227adca2a7d9285da182a3953f1295
Theerawat Pongsupawt (MBP) <rxseven.com@gmail.com>
2018-06-22 19:15:37 +0700

    Connect Root component to Redux store

--- src/components/core/Root/index.jsx
+++ src/components/core/Root/index.jsx
@@ -1,12 +1,20 @@
 // @flow
 // Module dependencies
 import * as React from 'react';
+import { Provider } from 'react-redux';
+
+import App from 'components/core/App';
 
 // Types
+type Props = { store: any };
 type Return = React.Node;
 
 // Component
-const Root = (): Return => <div>Root component</div>;
+const Root = ({ store }: Props): Return => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 // Module exports
 export default Root;
