abf68103e26bc4483e519718c3414dbe7f0df766
Theerawat Pongsupawt (MBP) <rxseven.com@gmail.com>
2018-06-22 19:06:27 +0700

    Create root Redux reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+// Module dependencies
+import { combineReducers } from 'redux-immutable';
+
+// Combine reducers
+const reducer = combineReducers({
+  data: () => null
+});
+
+// Module exports
+export default reducer;
