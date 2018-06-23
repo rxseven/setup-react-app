cd6618b1109f8e19b7c8315a8c3d83fd61176606
Theerawat Pongsupawt (MBP) <rxseven.com@gmail.com>
2018-06-23 07:56:32 +0700

    Create Spinner component

---
+++ src/components/common/Spinner/index.jsx
@@ -0,0 +1,15 @@
+// @flow
+// Module dependencies
+import * as React from 'react';
+
+// Styles
+import './styles.scss';
+
+// Types
+type Return = React.Node;
+
+// Component
+const Spinner = (): Return => <div styleName="spinner">Loading...</div>;
+
+// Module exports
+export default Spinner;
